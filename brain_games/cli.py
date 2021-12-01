aba89b1f2be0ec03bdbd550799172e89ceb76754
Sergey <kvazar941@gmail.com>
2021-12-01 23:29:50 +0300

    Project 1, Step 4, corrected files __init__.py, cli.py from folder brain_games

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,13 @@
+"""Communication module."""
 import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print('Hello, ' + name + '!')
+    """
+    Request a name and greet a user by name.
 
+    Returns:
+        str
+    """
+    name = prompt.string('May I have your name? ')
+    return 'Hello, {0}! '.format(name)
