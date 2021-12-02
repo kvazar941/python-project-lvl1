caa313403a12e3a1ba059dac4e31c0be0d6589b0
Sergey <kvazar941@gmail.com>
2021-12-02 13:56:58 +0300

    Project 1, Step 4, corrected file from brain_games/scripts/

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""main module with entry point."""
 from brain_games import cli
 
 
-
 def main():
-    print('Welcome to the Brain Games!')
-    cli.welcome_user()
+    """
+    Execute the main code.
+
+    Returns:
+        str
+    """
+    return 'Welcome to the Brain Games!\n{0}'.format(cli.welcome_user())
+
 
 if __name__ == '__main__':
     main()
