c2605db2e83c972affb51eeb817ae80670eea735
Sergey <kvazar941@gmail.com>
2021-12-07 01:11:21 +0300

    Project 1, Step 5, corrected WPS

--- brain_games/parity_check.py
+++ brain_games/parity_check.py
@@ -2,6 +2,11 @@
 import prompt
 import logging
 from random import randint
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+CONSOLEHANDLER = logging.StreamHandler()
+CONSOLEHANDLER.setLevel(logging.INFO)
+logger.addHandler(CONSOLEHANDLER)
 
 
 def game_parity_check():
@@ -11,21 +16,13 @@ def game_parity_check():
     Returns:
         str
     """
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(logging.INFO)
-    logger.addHandler(consoleHandler)
-    formatter = logging.Formatter('%(message)s')
-    consoleHandler.setFormatter(formatter)
     logger.info('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     logger.info('Hello, {0}!'.format(name))
     logger.info('Answer "yes" if the number is even, otherwise answer "no".')
     counter_question = 1
-    number_question = 3
-    while counter_question <= number_question:
-        random_number = randint(1, 100)
+    while counter_question <= 3:
+        random_number = randint(1, 10)
         if random_number % 2 == 0:
             parity = 'yes'
         else:
@@ -35,8 +32,7 @@ def game_parity_check():
         if answer == parity:
             logger.info('Correct!')
         else:
-            return "'{0}' is wrong answer ;(. Correct answer was '{1}'.\nLet,s try again, {2}!".format (answer, parity, name)
+            return "'{0}' is wrong answer ;(. Correct answer was '{1}'\nLet,s try again, {2}!'".format(answer, parity, name)
         counter_question += 1
 
     return 'Congratulation, {0}!'.format(name)
-
