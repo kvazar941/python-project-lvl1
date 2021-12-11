1edb43f7e8f4ba99df54c504ec1066cf900eeac2
Sergey <kvazar941@gmail.com>
2021-12-11 17:19:59 +0300

    Project 1 Step 6, add brain-calc, wps - ok, add asciinema, complete

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 href="https://codeclimate.com/github/codelimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 
 ![example workflow](https://github.com/kvazar941/python-project-lvl1/actions/workflows/workflows_make_lint.yml/badge.svg)
+
+<a href="https://asciinema.org/a/QavlAH4ZMJItSsO1vioKhacbL" target="_blank"><img src="https://asciinema.org/a/QavlAH4ZMJItSsO1vioKhacbL.svg" /></a>
+
+<a href="https://asciinema.org/a/tiCpjfEXKdWD3Pet6fxMSgqMG" target="_blank"><img src="https://asciinema.org/a/tiCpjfEXKdWD3Pet6fxMSgqMG.svg" /></a>
--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,7 +4,7 @@ import random
 from brain_games.game_logic import games_logic
 
 rules_game = 'What is the result of the expression?.'
-expres = []
+expr = []
 cor_answ = []
 
 
@@ -19,14 +19,19 @@ def main():
     while counter_question < games_logic.number_of_rounds:
         rand_num_one = random.SystemRandom().randint(1, 100)
         rand_num_two = random.SystemRandom().randint(1, 100)
-        operand = random.SystemRandom().choice('+', '-', '*')
-        expres.append('{0} {1} {2}'.format(rand_num_one, operand, rand_num_two))
-        if rand_num_one % 2 == 0:
-            cor_answ.append('yes')
-        else:
-            cor_answ.append('no')
+        operator = random.SystemRandom().choice('+-*')
+        expr.append('{0} {1} {2}'.format(rand_num_one, operator, rand_num_two))
+        if operator == '+':
+            res = rand_num_one + rand_num_two
+            cor_answ.append(str(res))
+        elif operator == '-':
+            res = rand_num_one - rand_num_two
+            cor_answ.append(str(res))
+        elif operator == '*':
+            res = rand_num_one * rand_num_two
+            cor_answ.append(str(res))
         counter_question += 1
-    return games_logic.main(rules_game, expres, cor_answ)
+    return games_logic.main(rules_game, expr, cor_answ)
 
 
 if __name__ == '__main__':
