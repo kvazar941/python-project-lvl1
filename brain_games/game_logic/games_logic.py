daff2d269bd096e8166865ba70408b86629341d9
Sergey <kvazar941@gmail.com>
2021-12-16 16:31:57 +0300

    corrected games_logic.py

--- brain_games/game_logic/games_logic.py
+++ brain_games/game_logic/games_logic.py
@@ -1,5 +1,4 @@
 """Game logic module."""
-# flake8: noqa
 import prompt
 
 number_of_rounds = 3  # 3 - the number of stages of the game
@@ -13,37 +12,31 @@ def main(rules, questions, correct_answers):
         rules: str
         questions: list
         correct_answers: list
-
-    Returns:
-        str
     """
-    # Welcome the player and set the rules of the game
-    print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')  # noqa: WPS421
     name = prompt.string('May I have your name? ')
-    print('Hello, {0}!'.format(name))
-    print(rules)
+    print('Hello, {0}!'.format(name))  # noqa: WPS421
+    print(rules)  # noqa: WPS421
     counter_question = 0
     while counter_question < number_of_rounds:
-        print('Question:', questions[counter_question])  # noqa: WPS421
-        # Get the player's response
-        answer = prompt.string('Your answer: ')
-        # Compare the player's response
-        # With the value of the 'parity' variable
-        if answer == correct_answers[counter_question]:
-            # Report the correct answer
-            print('Correct!')
+        print(  # noqa: WPS4
+            ("'Question: {0}'"
+             ).format(questions[counter_question]),
+        )
+        answer_user = prompt.string('Your answer: ')
+        if answer_user == correct_answers[counter_question]:
+            print('Correct!')  # noqa: WPS421
             counter_question += 1
             if counter_question == number_of_rounds:
-                print('Congratulations, {0}!'.format(name))
+                print('Congratulations, {0}!'.format(name))  # noqa: WPS421
         else:
-            # report an incorrect answer and end the game
-            print(
+            print(  # noqa: WPS421
                 ("'{0}' is wrong answer ;(. "
-                + "Correct answer was '{1}'.\n"  # noqa: W503
-                + "Let\'s try again, {2}!"  # noqa: W503
-                ).format(answer, correct_answers[counter_question], name)
+                 + "Correct answer was '{1}'.\n"  # noqa: W503
+                 + "Let\'s try again, {2}!"  # noqa: W503
+                 ).format(answer_user, correct_answers[counter_question], name),
             )
-            counter_question = number_of_rounds 
+            counter_question = number_of_rounds
 
 
 if __name__ == '__main__':
