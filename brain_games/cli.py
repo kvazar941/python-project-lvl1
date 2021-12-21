e053e0186d3c5bd112025edc605dbf8bac14bbda
Sergey <kvazar941@gmail.com>
2021-12-21 14:17:26 +0300

    corrected file cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,7 +7,3 @@ def welcome_user():
     print('Welcome to the Brain Games!')  # noqa: WPS421
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))  # noqa: WPS421
-
-
-if __name__ == '__main__':
-    welcome_user()
