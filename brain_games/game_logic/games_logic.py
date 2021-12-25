bbdb36c54363dac31e9bb324d6ee0dc2915f4e7f
Sergey <kvazar941@gmail.com>
2021-12-25 23:04:09 +0300

    refactoring games_logic

--- brain_games/game_logic/games_logic.py
+++ brain_games/game_logic/games_logic.py
@@ -3,7 +3,6 @@ import prompt
 
 NUMBER_OF_ROUNDS = 3  # 3 - the number of stages of the game
 ERROR_MESSAGE = "'{0}' is wrong answer ;(. Correct answer was '{1}'."
-NAME = ''
 
 
 def message(formation, formatting_message):
@@ -17,14 +16,6 @@ def message(formation, formatting_message):
     print(formation.format(*formatting_message))
 
 
-def welcome_user():
-    """Greet the player by name."""
-    message('{0}', ('Welcome to the Brain Games!',))
-    NAME = prompt.string('May I have your name? ')
-    message('Hello, {0}!', (NAME,))
-
-
-
 def launch_game(rules, question_answer):
     """
     Implement the logic of the module.
@@ -33,7 +24,9 @@ def launch_game(rules, question_answer):
         rules: str
         question_answer: function-generator
     """
-    welcome_user()
+    message('{0}', ('Welcome to the Brain Games!',))
+    name = prompt.string('May I have your name? ')
+    message('Hello, {0}!', (name,))
     message('{0}', (rules,))
     counter_question = 0
     while counter_question < NUMBER_OF_ROUNDS:
@@ -44,8 +37,8 @@ def launch_game(rules, question_answer):
             message('{0}', ('Correct!',))
             counter_question += 1
             if counter_question == NUMBER_OF_ROUNDS:
-                message('Congratulations, {0}!', (NAME,))
+                message('Congratulations, {0}!', (name,))
         else:
             message(ERROR_MESSAGE, (answer_user, answer))
-            message("Let\'s try again, {0}!", (NAME,))
+            message("Let\'s try again, {0}!", (name,))
             break
